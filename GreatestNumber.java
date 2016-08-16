f1981f343c7af726890e1eb81ed81a66a2b7eba1
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2016-08-16 21:33:01 +0530

    Add files via upload

---
+++ GreatestNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner=new Scanner(System.in);
+int a=scanner.nextInt();
+int b=scanner.nextInt();
+int c=scanner.nextInt();
+if(a>b&& a>c){
+	System.out.println("a is greater");
+}else if(b>c){
+	System.out.println("b is greater");
+}else{
+	System.out.println("c is greater");
+}
+	}
+
+}
