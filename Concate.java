d08a0ded4e308a1682761ba3f0b9774fdb799e61
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2016-08-16 22:29:24 +0530

    Add files via upload

---
+++ Concate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Concate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+String b=s.nextLine();
+String c=a.concat(b);
+System.out.println(c);
+	}
+
+}
